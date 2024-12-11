ee99f600746bdb21addce69800a4ffe240ad5e79
Marlon Harrison <marlon@ionic.io>
2024-12-11 14:30:15 -0600

    update homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonButton, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IABService } from '../services/iab.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  private readonly iabService = inject(IABService);
+
+  async iab(target: '_self' | '_blank' | '_system' | '_top' | '_parent' = '_blank') {
+    if(target === '_system') {
+    await this.iabService.openSystemBrowser('https://ionic.io');
+  } else {
+    await this.iabService.openInAppBrowser('https://ionic.io');
+  }
+}
 }
